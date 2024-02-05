bb968303aaf48b8f405656c0f81355edea3a9a51
Stark <mohd47149@gmail.com>
2024-02-06 00:18:52 +0530

    js bugs fix and then create some bugs f***

---
+++ main.js
@@ -0,0 +1,110 @@
+let index = 0,
+head_index = 0;
+const bg_images = document.querySelectorAll('.bg_images img');
+const dots = document.querySelectorAll('dot');
+const heading = document.querySelector('.heading');
+const head = document.querySelectorAll('.heading h1');
+const info = document.querySelector('.info');
+const para = document.querySelectorAll('.info p');
+const cards = document.querySelectorAll('.cards');
+const card = document.querySelectorAll('.card img');
+const slider = document.querySelector('.card_container');
+const next_btn = document.getElementById('next_btn');
+const prev_btn = document.getElementById('prev_btn');
+
+const bg_changer = () => {
+    for(let bg_img = 0; bg_img<bg_images.length; bg_img++){
+        bg_images[bg_img].style.opacity = 0;
+        bg_images[bg_img].style.transition = '0.5s'
+    }
+    bg_images[index-1].style.opacity = 1;
+    bg_images[index-1].style.transition = '0.5s'
+}
+
+const head_first_clone = head[0].cloneNode(true);
+const para_first_clone = para[0].cloneNode(true);
+const cards_first_clone = cards[0].cloneNode(true);
+const head_last_clone = head[head.length-1].cloneNode(true);
+const para_last_clone = para[para.length-1].cloneNode(true);
+const cards_last_clone = cards[cards.length-1].cloneNode(true);
+head_first_clone.id = 'fclone';
+head_last_clone.id = 'lclone';
+cards_last_clone.id = 'cl';
+slider.append(cards_first_clone)
+heading.append(head_first_clone);
+info.append(para_first_clone);
+heading.prepend(head_last_clone);
+info.prepend(para_last_clone);
+slider.prepend(cards_last_clone);
+
+let head_height = head[head_index].clientHeight;
+let para_height = para[head_index].clientHeight;
+let slide = cards[head_index].clientWidth;
+// console.log(slide)
+
+heading.addEventListener('transitionend',()=>{
+    let head = document.querySelectorAll('.heading h1');
+    if(head[head_index].id === head_first_clone.id){
+        heading.style.transition='none';
+        info.style.transition='none';
+        head_index = 1;
+        heading.style.transform = `translateY(-${head_index*head_height}px)`;
+        info.style.transform = `translateY(-${head_index*para_height}px)`;
+        console.log('success' + head[head_index].id);
+    }
+    
+    if(head[head_index].id === head_last_clone.id){
+        heading.style.transition='none';
+        head_index = 1;
+        // heading.cloneNode(true);
+        heading.style.transform = `translateY(-${head_index*head_height}px)`;
+        console.log('success2' + head[head_index].id);
+    }
+})
+
+const head_changer_nxt = () => {
+    let head = document.querySelectorAll('.heading h1');
+    let cards = document.querySelectorAll('.cards');
+    if(head_index>=head.length-1) return;
+    head_index++;
+    heading.style.transform = `translateY(-${head_index*head_height}px)`;
+    info.style.transform = `translateY(-${head_index*para_height}px)`;
+    slider.style.transform = `translateX(-${head_index*slide}px)`;
+    cards[index-1].classList.remove('small');
+    cards[index].classList.add('big');
+    slider.style.transition = `1s`;
+    heading.style.transition = `1s`;
+    info.style.transition = `1s`;
+    // console.log(head.length)
+    console.log(head_index)
+}
+
+const head_changer_prv = () => {
+    let cards = document.querySelectorAll('.cards');
+    if(head_index <= 0) return;
+    head_index--;
+    heading.style.transform = `translateY(-${head_index*head_height}px)`;
+    info.style.transform = `translateY(-${head_index*para_height}px)`;
+    slider.style.transform = `translateX(-${head_index*slide}px)`;
+    cards[index-1].classList.remove('big');
+    cards[index].classList.add('small');
+    slider.style.transition = `1s`;
+    heading.style.transition = `1s`;
+    info.style.transition = `1s`;
+    // console.log(head.length)
+    console.log(head_index)
+}
+
+next_btn.addEventListener('click',()=>{
+    index++;
+    if(index>bg_images.length){
+        index = 1;
+    }
+    head_changer_nxt();
+    bg_changer();
+    // console.log(index)
+});
+
+prev_btn.addEventListener('click',()=>{
+    head_changer_prv();
+})
